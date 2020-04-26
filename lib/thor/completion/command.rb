2f273e8163467de1db63231f67a66db53decf26e
Pierre Neyron <pierre.neyron@imag.fr>
2020-04-27 00:11:12 +0200

    Fix the completion command for complete -C

--- lib/thor/completion/command.rb
+++ lib/thor/completion/command.rb
@@ -9,12 +9,13 @@ class Thor
           method_option :name, desc: 'Tool name', type: :string
           method_option :line, desc: 'Command line to complete (as set in the $COMP_LINE environment variable)',
                                type: :string
-          def completion
+          def completion(*_args) # when called by `complete -C <command>`, 3 useless arguments a passed
             name = options.name || File.basename($PROGRAM_NAME)
-            line = options.line.nil? ? ENV['COMP_LINE'] : "#{name} #{options.line}"
             if options.dump
               puts Completion::Introspector.new(self.class, name)
             else
+              line = options.line.nil? ? ENV['COMP_LINE'] : "#{name} #{options.line}"
+              raise Completion::Error, 'Completion line is not set' if line.nil?
               generator = Completion::Generator.new(self.class, name)
               puts generator.match(line)
             end
