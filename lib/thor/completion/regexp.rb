3c77f0cc457a320627ac0f10a7d02eddcdf985d4
Pierre Neyron <pierre.neyron@imag.fr>
2020-04-26 19:50:47 +0200

    Rework "static class"

--- lib/thor/completion/regexp.rb
+++ lib/thor/completion/regexp.rb
@@ -2,20 +2,21 @@ class Thor
   module Completion
     # Regexp build helper
     module Regexp
-      def self.build(str)
-        build_rec(str)
+      class <<self
+        def build_rec(str, regexp_str = '')
+          return /^#{regexp_str}$/ if str.empty?
+          build_rec(str[0..-2], "(?:#{escape_char(str[-1])}#{regexp_str})?")
+        end
+
+        def escape_char(char)
+          to_escape = ['?', '*', '+', '(', ')', '{', '}', '[', ']', '^', ':', '!', '|', '\\', '-', '\$', '#']
+          return "\\#{char}" if to_escape.include?(char)
+          char
+        end
+
+        alias build build_rec
       end
 
-      def self.build_rec(str, regexp_str = '')
-        return /^#{regexp_str}$/ if str.empty?
-        build_rec(str[0..-2], "(?:#{escape_char(str[-1])}#{regexp_str})?")
-      end
-
-      def self.escape_char(char)
-        to_escape = ['?', '*', '+', '(', ')', '{', '}', '[', ']', '^', ':', '!', '|', '\\', '-', '\$', '#']
-        return "\\#{char}" if to_escape.include?(char)
-        char
-      end
       private_class_method :build_rec, :escape_char
     end
   end
