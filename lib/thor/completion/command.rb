3bcf2cd95afa0b2cbd9e72813b3c6e4be6c13f28
Pierre Neyron <pierre.neyron@imag.fr>
2020-04-26 19:51:10 +0200

    Allow passing COMP_LINE in options

--- lib/thor/completion/command.rb
+++ lib/thor/completion/command.rb
@@ -4,17 +4,19 @@ class Thor
     module Command
       def self.included(klass)
         klass.class_eval do
-          desc 'completion', 'Print completion'
+          desc 'completion', 'Print completion', hide: true
           method_option :dump, desc: 'List all possible commands', type: :boolean
-          method_option :name, desc: 'Command name', type: :string
+          method_option :name, desc: 'Tool name', type: :string
+          method_option :line, desc: 'Command line to complete (as set in the $COMP_LINE environment variable)',
+                               type: :string
           def completion
             name = options.name || File.basename($PROGRAM_NAME)
+            line = options.line || ENV['COMP_LINE']
             if options.dump
               puts Completion::Introspector.new(self.class, name)
             else
               generator = Completion::Generator.new(self.class, name)
-              comp_line = ENV['COMP_LINE']
-              puts generator.match(comp_line)
+              puts generator.match(line)
             end
           end
         end
